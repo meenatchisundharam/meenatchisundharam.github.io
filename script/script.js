92fa642b8c864d811684b603435a926c40589bca
meenatchisundharam <121151601+meenatchisundharam@users.noreply.github.com>
2023-11-07 11:31:34 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -10,3 +10,47 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+function validateForm() {
+    const name = document.querySelector('#name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const subject = document.querySelector('#subject').value.trim();
+    const message = document.querySelector('#message').value.trim();
+    const contactMeButton = document.querySelector('#contactMeButton');
+
+    if (name.length > 0 && email.length > 0 && subject.length > 0 && message.length > 0) {
+        // Enable the button if all required fields are filled
+        contactMeButton.disabled = false;
+    } else {
+        // Disable the button if any required fields are empty
+        contactMeButton.disabled = true;
+    }
+}
+
+function sendEmail() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "sundharmadhim@gmail.com",
+        Password: "74B41D3A06829DDDCF3045314100F0743E05",
+        To: 'sundharmadhim@gmail.com',
+        From: 'sundharmadhim@gmail.com',
+        Subject: subject,
+        Body: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    }).then(
+        message => {
+            alert('Email sent successfully');
+            // Optionally, clear the form fields
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('subject').value = '';
+            document.getElementById('message').value = '';
+        },
+        error => {
+            alert('Email could not be sent: ' + error);
+        }
+    );
+}
